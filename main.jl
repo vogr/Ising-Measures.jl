e57c48548d04a20db0f3b1f347d17cd7fe35d974
vogier <contact@vogier.fr>
2018-06-04 21:22:28 +0200

    First working commit.

--- main.jl
+++ main.jl
@@ -4,71 +4,138 @@ using Memento
 using Plots
 Plots.gr()
 using ProgressMeter
+using DataStructures
 
 dmodules = now() - tmod
 
+const J = 1 # Neighbor interraction
+const k = 1 # Boltzmann's constant
+const dynamic = "local"
 const ρ = 0.4 # spins down ratio
-const N = 120 # Lattice size
+const N = 20 # Lattice size
 const N2 = N^2
 const directions = CartesianIndex.([(0,1), (0,-1), (1,0), (-1,0)])
 
+
 const SEED = 0x5e6c1f2e
 
-const eq_steps = 10^3
-const mc_steps = 9 * 10^3
+const eq_steps = 100
+const mc_steps = 900
+const plot_every = 10
 
 function main()
   logger = Memento.config("debug"; fmt="[{level} | {name}]: {msg}")
 
+  info(logger, "Loaded modules in $dmodules")
+
   info(logger, "Seed: $SEED")
   prng = MersenneTwister(SEED)
 
-  const tT = linspace(0.1, 6, 2)
-  const outdir = "simulations/kawasaki_$SEED"
+  const tT = linspace(0.1, 6, 20)
+  const outdir = "simulations/kawasaki_$(SEED)_$(N)"
   mkpath(outdir)
 
   
-  ω = init_lattice(prng; is_local=true)
-  p0 = make_heatmap(ω)
+  model = init_model(prng; is_local=true)
+
+  t0 = now() 
+  p0 = make_heatmap(model.ω)
   plot!(p0, title="Initial lattice (t=0)")
   savefig(p0, "$outdir/image0.svg")
+  dfirstplot = now() - t0
+  info(logger, "Time for first plot: $dfirstplot")
 
   tE = similar(tT)
   tE2 = similar(tT)
   tc = similar(tT)
 
-  @showprogress for (i_T,T) in enumerate(tT)
+  @showprogress 1 "Computing..." for (i_T,T) in enumerate(tT)
+    info(logger, "Starting T=$T")
     subdir = "$outdir/$T"
     mkpath(subdir)
 
+    β = 1 / (k * T)
     totE = 0
     totE2 = 0
     t = 0
-    for k in 1:eq_steps
-      sweep(ω, T, prng)
-      t = t + 1
+
+    qsize = 10
+    durations = Queue(Int)
+
+    function timed_sweep()
+      h1 = now()
+      for _ in 1:plot_every
+        mcmove(model, β, prng)
+        t = t + 1
+      end
+      h2 = now()
+      d = h2 - h1
+      enqueue!(durations, Dates.value(d))
+      if length(durations) > qsize
+        dequeue!(durations)
+      end
     end
 
-    for k in 1:mc_steps
-      p = make_heatmap(ω)
-      plot!(p, title="T=$T, t=$t")
-      savefig(p, "$subdir/image$(dec(t, 9)).svg")
-      sweep(ω, T, prng)
-      t = t + 1
+		h0 = now()
+    # Reach a first equilibrium
+    info(logger, "Reaching a first equilibrium...")
+    for i in 1:eq_steps
+      timed_sweep()
+      if i % plot_every == 0
+        hplot = now()
+        p = make_heatmap(model.ω)
+        plot!(p, title="T=$T, t=$t (eq.)")
+        savefig(p, "$subdir/image$(dec(t, 9)).svg")
+        dplot = now() - hplot
 
-      E = H(ω)
+        total_time = Dates.canonicalize(Dates.CompoundPeriod(now() - h0))
+        it_per_s = durations_to_itps(durations)
+        message = """
+        ($total_time) T=$(T) ; t=$(t)
+        \t$plot_every iterations in $(front(durations)). On average (last $qsize): $it_per_s it/s.
+        \t+$dplot to plot.
+        """
+        info(logger, message)
+      end
+    end
+
+    # MC moves between measures
+    for i in 1:mc_steps
+      timed_sweep()
+
+      henergy = now()
+      E = H(model.ω)
       totE = totE + E
       totE2 = totE2 + E*E
+      denergy = now() - henergy
+
+      if i % plot_every == 0
+        hplot = now()
+        p = make_heatmap(model.ω)
+        plot!(p, title="T=$T, t=$t, E=$E")
+        savefig(p, "$subdir/image$(dec(t, 9)).svg")
+        dplot = now() - hplot
+
+        total_time = Dates.canonicalize(Dates.CompoundPeriod(now() - h0))
+        it_per_s = durations_to_itps(durations)
+        message = """
+        ($total_time) T=$(T) ; t=$(t)
+        \t$plot_every iterations in $(front(durations)). On average (last $qsize): $it_per_s it/s.
+        \t+$dplot to plot.
+        \t+$denergy to compute energy.
+        """
+        info(logger, message)
+      end
     end
 
     mE = 1 / mc_steps * totE
     mE2 = 1 / mc_steps * totE2
-    tE[iT] = mE
-    tE2[iT] = mE2
-    tc[iT] = k * β^2 * (mE2 - mE^2) / N2
+    tE[i_T] = mE
+    tE2[i_T] = mE2
+    tc[i_T] = k * β^2 * (mE2 - mE^2) / N2
   end
 
-  l = @layout [  grid(1,2)]
+  l = @layout [grid(1,2)]
   pE = plot(tT, tE)
   plot!(pE; xlabel="T", ylabel="E")
   plot!(pE; legend=false)
@@ -76,13 +143,17 @@ function main()
   pc = plot(tT, tc; xlabel="T", ylabel = "c")
   plot!(pc; legend=true)
 
-  p = plot(pE,pclayout=l)
+  p = plot(pE,pc,layout=l)
   savefig(p, "$outdir/mesures$N.svg") 
 end
 
+function durations_to_itps(durations)
+  it_per_ms = (plot_every * length(durations)) / sum(durations)
+  it_per_s = round(it_per_ms * 1000, 4)
+	it_per_s
+end
 
 function H(ω)
-  J = 1
   E = 0
   for I in CartesianRange(CartesianIndex(2,2), CartesianIndex(N-1,N-1))
     s = ω[I]
@@ -96,6 +167,9 @@ end
 
 
 
+struct LocalModel
+  ω::Array{Int,2}
+end
 
 
 struct NonlocalModel
@@ -104,7 +178,11 @@ struct NonlocalModel
   spins_up_coords::Array{ Tuple{Int,Int}, 1 }
 end
 
-function init_lattice(prng; is_local=true)
+struct MetropolisModel
+  ω::Array{Int,2}
+end
+
+function init_model(prng; is_local=true)
   mutable_sites_coordinates = [CartesianIndex(i,j) for i in 3:(N-2) for j in 3:(N-2)]
   #mutable_sites_coordinates = CartesianRange(CartesianIndex(3,3), CartesianIndex(N-2),(N-2))
   distrib = shuffle(prng, mutable_sites_coordinates)
@@ -115,10 +193,12 @@ function init_lattice(prng; is_local=true)
   for X in spins_down_coords
     ω[X] = -1
   end
-  if is_local
-    ω
-  else
+  if dynamic == "local"
+    LocalModel(ω)
+  elseif dynamic == "nonlocal"
     NonlocalModel(ω, spins_down_coords, spins_up_coords)
+  elseif dynamic == "metropolis"
+    MetropolisModel(ω)
   end
 end
 
@@ -129,7 +209,7 @@ function random_indexes(prng, n)
 end
 
 function random_coords(prng, n)
-  CartesianIndex.(zip(random_indexes(prng, n), random_indexes(prng, n)))
+  CartesianIndex.(collect(zip(random_indexes(prng, n), random_indexes(prng, n))))
 end
 
 function delta_neighbors(ω, X)
@@ -140,17 +220,13 @@ function delta_neighbors(ω, X)
   s
 end
 
-function sweep(ω, T, prng)
-  for i in 1:N2
-    kawasaki_local_move(ω, T, prng)
-  end
+function mcmove(model::LocalModel, β, prng)
+  kawasaki_local_move(model.ω, β, prng)
 end
 
-function kawasaki_local_move(ω, T, prng)
-  const k = 1
-  const β = 1 / (k * T)
+function kawasaki_local_move(ω, β, prng)
   const boltzmann_quotient = Dict([(i, exp(-i * β)) for i in -20:20])
-  const RANDOM_COORDS = random_coords(model.lattice, N2)
+  const RANDOM_COORDS = random_coords(prng, N2)
   for X1 in RANDOM_COORDS
     spin1 = ω[X1]
     dX = rand(prng, directions)
@@ -162,8 +238,8 @@ function kawasaki_local_move(ω, T, prng)
                2 * spin1
       dE = - 2 * spin1 * dneighbors
       if dE <= 0 || get(boltzmann_quotient, dE, nothing) >= rand(prng)
-        model.lattice.sites[X1] = spin2
-        model.lattice.sites[X2] = spin1
+        ω[X1] = spin2
+        ω[X2] = spin1
       end
     end
   end
